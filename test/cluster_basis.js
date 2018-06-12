d9e6a7e1deb25717dc94c72c49293741b6eb0f3a
aitbenmouh <lhoussaine.aitbenmouh@wakanda.io>
2018-06-12 14:17:15 +0000

    fixed test

--- test/cluster_basis.js
+++ test/cluster_basis.js
@@ -1,158 +1,157 @@
-const assert = require('assert');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const storageMgr = require('..');
+var expect = require('expect.js');
 
-var storage = null;
-var stoppedWorkersCount = 0;
+if (cluster.isMaster) {
 
+    describe('Wakanda Storage Cluster', function() {
 
-var realCode = function realCode ( callback) {
-		
-		if (cluster.isMaster) {
-			
-			var exitTimeout = null;
-			var stoppedWorkersInterval = null;
-			
-			var beforeExit = function beforeExit() {
-				
-				var workersCount = storage.get('workersCount');
-				var workersInfos = storage.get('workersInfos');
-				
-				var findWorkerInfos = function findWorkerInfos(id) {
-					var infos = null;
-					for (var iter = 0, len = workersInfos.length ; (iter < len) && (infos == null) ; ++iter) {
-						if (workersInfos[iter].id == id) {
-							infos = workersInfos[iter];
-						}
-					}
-					return infos;
-				};
+        var storage = null;
 
-				for (const id in cluster.workers) {
-					var infos = findWorkerInfos(id);
-					if (infos == null) {
-						throw (new Error('cannot found infos for worker ' + id.toString()));
-					}
-				}
-				
-				
+        before(function() {
+            storage = storageMgr.create('masterSharedStorage');
+        });
 
-				storageMgr.destroy('masterSharedStorage');
-				process.exit();
-			
-			}
-			
-			var exitTimeoutCallback = function exitTimeoutCallback() {
-				clearInterval(stoppedWorkersInterval);
-				cluster.disconnect(beforeExit);
-			};
-			
-			var checkForStoppedWorkers = function checkForStoppedWorkers() {
-				
-				if (stoppedWorkersCount == numCPUs) {
-					clearTimeout(exitTimeout);
-					clearInterval(stoppedWorkersInterval);
-					beforeExit();
+        it("check cluster behaviour", function() {
+            var testPromise = new Promise(function(resolve, reject) {
+                var exitTimeout = null;
+                var stoppedWorkersInterval = null;
+                var stoppedWorkersCount = 0;
 
-				}
-			};
+                var checkForStoppedWorkers = function checkForStoppedWorkers() {
+                    if (stoppedWorkersCount == numCPUs) {
+                        clearTimeout(exitTimeout);
+                        clearInterval(stoppedWorkersInterval);
+                        resolve();
+                    }
+                };
 
-			for (let i = 0; i < numCPUs; i++) {
-				cluster.fork();
-			}
+                for (let i = 0; i < numCPUs; i++) {
+                    cluster.fork();
+                }
 
-			for (const id in cluster.workers) {
-				cluster.workers[id].on('message', function(msg) {
-					if (msg.name && msg.name == 'WORKER_WILL_STOP') {
-						++stoppedWorkersCount;
-					}
+                for (const id in cluster.workers) {
+                    cluster.workers[id].on('message', function(msg) {
+                        if (msg.name && msg.name == 'WORKER_WILL_STOP') {
+                            ++stoppedWorkersCount;
+                        }
+                    });
+                }
+
+                stoppedWorkersInterval = setInterval(checkForStoppedWorkers, 500);
+                exitTimeout = setTimeout(resolve, 10000);
+            });
+
+            return testPromise.then(function(){
+                var workersCount = storage.get('workersCount');
+                describe('count workers', function() {
+					it('#should return number ', function() {
+						expect(workersCount).to.be.a('number');
+					});
 				});
-			}
+				
+				describe('workers count and CPU', function() {
+					it('#should be equal ', function() {
+						expect(workersCount).to.be(numCPUs);
+					});
+				});
+                
 
-			stoppedWorkersInterval = setInterval(checkForStoppedWorkers, 500);
-			exitTimeout = setTimeout(beforeExit, 10000);
-			
-			
-			
-		
-		
-		}else {
-			
-			storage = storageMgr.get('masterSharedStorage');
-			storage.lock()
-			
-			var workersCount = storage.get('workersCount');
-			if (typeof(workersCount) == 'undefined') {
-				workersCount = 1;
-			}else {
-				++workersCount;
-			}
-			
-			storage.set('workersCount', workersCount);
-			
-			var infos = {
-				'id': cluster.worker.id,
-				'pid': process.pid,
-				'type': 'worker'
-			};
-			
-			var workersInfos = storage.get('workersInfos');
-			
-			if (typeof(workersInfos) == 'undefined') {
-				workersInfos = [infos];
-			}else {
-				workersInfos.push(infos);
-			}
-			
-			storage.set('workersInfos', workersInfos);
-			
-			storage.unlock();
+                var workersInfos = storage.get('workersInfos');
+				describe('workers info type', function() {
+					it('#should be an array ', function() {
+						expect(workersInfos).to.be.an('array');
+					});
+				});
+                
+				describe('workers info and number of CPU', function() {
+					it('#should be equal ', function() {
+						expect(workersInfos.length).to.be(numCPUs);         
+					});
+				});
+                
+                
+                      
 
-			process.send({
-				'name': 'WORKER_WILL_STOP',
-				'id': cluster.worker.id,
-				'pid': process.pid
-			});
+                var findWorkerInfos = function findWorkerInfos(id) {
+                    var infos = null;
+                    for (var iter = 0, len = workersInfos.length ; (iter < len) && (infos == null) ; ++iter) {
+                        if (workersInfos[iter].id == id) {
+                            infos = workersInfos[iter];
+                        }
+                    }
+                    return infos;
+                };
+
+                for (const id in cluster.workers) {
+                    var infos = findWorkerInfos(id);
+					describe('worker info', function() {
+						it('#should not be null ', function() {
+							expect(infos).not.to.be(null);        
+						});
+					});
+                
+                    
+                }
+            });
 			
-		}
-		callback();
-};
+        });        
 
-
-
-
+        after(function() {
+            storageMgr.destroy('masterSharedStorage');
+        });         
+    });
 	
-describe('Wakanda storage cluster', function() {
+}else {
     
-	// Before 
-	before(function(done) {
-		
-		storageMgr.destroy('masterSharedStorage');
-		storage = storageMgr.create('masterSharedStorage');
-		done();
-	});
-	
-	
-	describe('#check for stopped workers ', function() {
-		it('should return true', function(done) {
-			this.timeout(20000);
-			realCode(function (){
-				assert.equal(numCPUs, stoppedWorkersCount);  
-				done();
-			});
-			
+    var storage = storageMgr.get('masterSharedStorage');
+    storage.lock();
+	describe('open existing storage', function() {
+		it('#should be a valide object ', function() {
+			expect(storage).not.to.be(null);         
 		});
-		
 	});
-	
-	// End 	
-	after(function(done) {
-		storageMgr.destroy('storageMgr');
-		done();
+
+    var workersCount = storage.get('workersCount');
+    if (typeof(workersCount) == 'undefined') {
+        workersCount = 1;
+    }
+    else {
+        ++workersCount;
+    }
+	describe('count workers', function() {
+		it('#should at least get one worker ', function() {
+			expect(workersCount).not.to.be(0);         
+		});
 	});
-	
-});
 
+    storage.set('workersCount', workersCount);
 
+    var infos = {
+        'id': cluster.worker.id,
+        'pid': process.pid,
+        'type': 'worker'
+    };
+    var workersInfos = storage.get('workersInfos');
+    if (typeof(workersInfos) == 'undefined') {
+        workersInfos = [infos];
+    }
+    else {
+        workersInfos.push(infos);
+    }
+	describe('set workers info', function() {
+		it('#should not to be empty ', function() {
+			expect(workersInfos).to.not.be.empty();       
+		});
+	});
+    storage.set('workersInfos', workersInfos);
 
+    storage.unlock();
+
+    process.send({
+        'name': 'WORKER_WILL_STOP',
+        'id': cluster.worker.id,
+        'pid': process.pid
+    });
+}
